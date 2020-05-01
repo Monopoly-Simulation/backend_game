9b3afa02b99cb41a41ddf31117fbbc27d946bfc6
yx1215 <37573101+yx1215@users.noreply.github.com>
2020-05-02 02:50:40 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from game import *
-import random
-random.seed(0)
-
-g = Game([Player(1,1,1,1,0.5,0.5,0.5), Player(2,2,2,2,500,500,500), Player(3,0,0,0,500,500,500)], 1000)
-g.run()
